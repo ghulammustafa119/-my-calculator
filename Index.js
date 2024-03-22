f7fc83970d1ef3221160a8a13628b7deaa5dc0bd
ghulammustafa119 <ghulammustafabhutto@gmail.com>
2024-03-22 12:09:42 +0500

    tweak the text

--- Index.js
+++ Index.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "enter first number", type: "number", name: "firstnumber" },
-    { message: "enter second number", type: "number", name: "secondnumber" },
+    { message: "enter your first number", type: "number", name: "firstnumber" },
+    { message: "enter your second number", type: "number", name: "secondnumber" },
     {
         message: "slect one of the operation to perform operation",
         type: "list",
